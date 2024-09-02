4c56aa6e002ead7e50e55b9fb77cdf9a089948f9
ydabrilp <ydabrilp@ufpso.edu.co>
2024-09-02 01:28:22 -0500

    [6] - swagger implementation

--- src/main/java/com/emazon/msstock/adapters/driving/http/controller/ArticleRestControllerAdapter.java
+++ src/main/java/com/emazon/msstock/adapters/driving/http/controller/ArticleRestControllerAdapter.java
@@ -9,6 +9,11 @@ import com.emazon.msstock.domain.api.IArticleServicePort;
 import com.emazon.msstock.domain.model.Article;
 import com.emazon.msstock.domain.model.Category;
 import com.emazon.msstock.domain.model.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -29,6 +34,12 @@ public class ArticleRestControllerAdapter {
     private final IArticleRequestMapper articleRequestMapper;
     private final IArticleResponseMapper articleResponseMapper;
 
+    @Operation(summary = "Save an article", description = "Creates a new article in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Article successfully created"),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = String.class)))
+    })
     @PostMapping
     public ResponseEntity<Void> addArticle(@RequestBody AddArticleRequest request) {
         List<Category> categoryRequest = articleRequestMapper.articleToCategoryList(request);
@@ -38,6 +49,12 @@ public class ArticleRestControllerAdapter {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Get articles sorted by criteria", description = "Retrieves a list of articles sorted by name, brand name, or category name and paginated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Articles successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = String.class)))
+    })
     @GetMapping()
     public ResponseEntity<Pagination<ArticleResponse>> getAllArticles(
             @Min(value = 0, message = ValidateParametersConstants.PAGE_NUMBER_NEGATIVE)
